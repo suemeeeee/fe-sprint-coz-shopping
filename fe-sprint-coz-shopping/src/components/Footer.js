d686d4c7d8955fb8065c182e4af2f45301756797
suemeeeee <yungmi2909@gmail.com>
2023-05-13 15:35:44 +0900

    ✅ 초기 설정 및 Header & Footer 구현 (CSS완)

---
+++ fe-sprint-coz-shopping/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://www.codestates.com/terms/policy" target="_blank">
+          개인정보 처리방침 |
+        </a>
+        <a href="https://www.codestates.com/terms/policy" target="_blank">
+          이용 약관
+        </a>
+      </div>
+      <div>
+        <p>All rights reserved @ Codestates</p>
+      </div>
+    </footer>
+  );
+}
